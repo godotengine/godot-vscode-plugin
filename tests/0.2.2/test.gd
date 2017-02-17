6d16899437ab13cf713e8bd37127f8b5f8e6da6c
Koivulehto <koivuleht@protonmail.com>
2017-02-17 16:00:44 +0200

    Demonstration of current diagnostics issues
    All the lines that get a complaint from diagnostics are marked
    with #in 0.2.2 and a short explanation whether the complaint is valid
    or not.

---
+++ tests/0.2.2/test.gd
@@ -0,0 +1,60 @@
+#Highlights syntax highglighting issues in godot-tools release 0.2.2
+
+extends Node
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+#func read(): #in 0.2.2, false positive
+#	var path = "res://assets/instructions.toml"
+#	var file = File.new()
+#	file.open(path, file.READ)
+#	var content = file.get_as_text()
+#	if (content == ""):
+#		return "File doesn't exist or is empty."
+#	file.close()
+#	return content
+#
+#func _ready(): #in 0.2.2 false positive
+#	# Called every time the node is added to the scene.
+#	# Initialization here
+#	read()
+
+func remove_ends(text): #in 0.2.2 false positive
+#vasdfs
+	var result = text
+	result = result.replace("[", "")
+	result = result.replace("]", "")
+	return result
+
+func read_cfg(path): #in 0.2.2 false positive
+
+	var config = ConfigFile.new() 
+	var err = config.load(path)
+	
+	var sections = {}
+	if err == OK: # if not, something went wrong with the file loading
+	    # Look for the display/width pair, and default to 1024 if missing
+#	   	#var screen_width = get_value("display", "width", 1024) #in 0.2.2 false positive
+#	    # Store a variable if and only it hasn't been defined yet
+#	    if not config.has_section_key("audio", "mute"):
+#	        config.set_value("audio", "mute", false)
+#	    # Save the changes by overwriting the previous file
+#	    config.save("user://settings.cfg")
+		for i in config.get_sections():
+			var section_pairs = {}
+			for j in config.get_section_keys(i):
+				section_pairs[j] = config.get_value(i, j)
+			sections[i] = section_pairs
+	print(sections)
+	return sections
+
+func something(): #in 0.2.2 diagnostics correctly complains
+
+func something_else():
+	var s = 2 #in 0.2.2 diagnostics should complain
+	return 3
+
+func yet_else():
+	pass
