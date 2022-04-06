38aaae8ba6c3f2fa82e8580d03ff7341328d57bc
David Kincaid <daelonsuzuka@gmail.com>
2022-04-06 07:01:51 -0400

    Recreated branch from old repository

---
+++ syntaxes/examples/gdscript1.gd
@@ -0,0 +1,104 @@
+extends Node
+class_name TestClass
+
+# ******************************************************************************
+
+var a
+remote var b = 10.0
+remotesync var c := 20
+master var d :int = 30
+puppet var e :int
+
+# ------------------------------------------------------------------------------
+
+var f = 40 setget set_f
+func set_f(value):
+	pass
+
+var g:int setget set_g, get_g
+func set_g(value: int) -> void:
+	pass
+
+func get_g() -> int:
+	return 0
+
+# ------------------------------------------------------------------------------
+
+var h = "double quotes"
+var i = 'single quotes'
+var j = """
+triple double quotes
+"""
+# var k = '''triple single quotes''' # this should be red because it's invalid
+
+# ------------------------------------------------------------------------------
+
+var IS_CONSTANT
+var not_CONSTANT
+var ALSO_NOT_constant
+var CONSTANT_not
+
+# ------------------------------------------------------------------------------
+
+onready var node_a = $Child
+onready var node_b = $Child/GrandChild
+onready var node_c = $"../Sibling"
+onready var node_cc = $'../Sibling'
+onready var node_d = $'..' # parent
+onready var node_e = $"../.." # grandparent
+
+onready var node_f = get_node('Child')
+onready var node_g = get_node("Child/GrandChild")
+onready var node_h = get_node("../Sibling")
+
+onready var node_i = $badlyNamedChild
+onready var node_j = $badlyNamedChild/badly_named_grandchild
+
+var node_path_a = NodePath("Child")
+var node_path_b = NodePath('Child/GrandChild')
+var node_path_c = NodePath('../Sibling')
+
+# ------------------------------------------------------------------------------
+
+var _script = GDScript.new()
+
+var directions = [
+	Vector2.UP,
+	Vector2.DOWN,
+	Vector2.LEFT,
+	Vector2.RIGHT,
+]
+
+enum test_enum {
+	VALUE_1,
+	VALUE_2,
+	VALUE_3,
+}
+
+export(test_enum) var enum_variable = test_enum.VALUE_1
+
+# ------------------------------------------------------------------------------
+
+class InnerClass:
+	var some_var = 100
+	var dict = {
+		'key_a': some_var,
+		'key_b': str(10),
+		key_c = some_var,
+		key_d = int('10'),
+		key_e = Color(1, 1, 1),
+		key_f = Vector2(10, -10)
+	}
+
+	func _ready():
+		if true and true:
+			pass
+		elif false:
+			while true:
+				pass
+		else:
+			pass
+		
+		pass
+	
+
