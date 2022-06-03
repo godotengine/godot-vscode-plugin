c3a8a0698d70119779feefa1e868a5a289280ca2
oidro <31112680+oidro@users.noreply.github.com>
2022-06-03 17:21:37 -0400

    Fix unintended get_configuration behavior

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,11 @@ import * as vscode from "vscode";
 const CONFIG_CONTAINER = "godot-tools";
 
 export function get_configuration(name: string, default_value: any = null) {
-	return vscode.workspace.getConfiguration(CONFIG_CONTAINER).get(name, default_value) || default_value;
+	let config_value = vscode.workspace.getConfiguration(CONFIG_CONTAINER).get(name, null);
+	if (config_value === null) {
+		return default_value;
+	}
+	return config_value;
 }
 
 export function set_configuration(name: string, value: any) {
