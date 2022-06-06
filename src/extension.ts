f9ed7f633399e8a725985fcdc716cce914d422f5
oidro <31112680+oidro@users.noreply.github.com>
2022-06-06 13:22:32 -0400

    Show message on 1.x.x -> 2.x.x update

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,31 @@
-import { ExtensionContext } from "vscode";
+import { ExtensionContext, extensions, window } from "vscode";
 import { GodotTools } from "./godot-tools";
 import debuggerContext = require("./debugger/debugger_context");
 
 let tools: GodotTools = null;
 
+export function showUpdateMessage(context: ExtensionContext) {
+	const localVersion: string | undefined =
+		context.globalState.get("previousVersion");
+	const syncedVersion: string = extensions.getExtension(context.extension.id)!
+		.packageJSON.version;
+
+	// Store the current version of the extension, this persists across reloads & updates.
+	context.globalState.update("previousVersion", syncedVersion);
+
+	if (localVersion !== syncedVersion && syncedVersion.startsWith("2.")) {
+		if (localVersion === undefined || localVersion.startsWith("1.")) {
+			window.showInformationMessage(
+				`Version 2.0.0 of the Godot Tools extension renames various settings. Please update your settings file.`,
+				"OK"
+			);
+		}
+	}
+}
+
 export function activate(context: ExtensionContext) {
+	showUpdateMessage(context);
+
 	tools = new GodotTools(context);
 	tools.activate();
 	debuggerContext.register_debugger(context);
