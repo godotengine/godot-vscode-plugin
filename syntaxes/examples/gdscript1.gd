ec95c21b84bc585ae0c8002ea2a623ddd04521cd
Daelon Suzuka <daelonsuzuka@gmail.com>
2022-08-06 03:07:31 -0400

    Add highlighting support for %Unique nodes in NodePaths (#403)
    Co-authored-by: Hugo Locurcio <hugo.locurcio@hugo.pro>

--- syntaxes/examples/gdscript1.gd
+++ syntaxes/examples/gdscript1.gd
@@ -127,6 +127,27 @@ onready var node_h = get_node("../Sibling")
 if has_node('Child') and get_node('Child').has_node('GrandChild'):
 	pass
 
+#! NOTE: scene unique nodes can only appear inside quoted nodepaths, not
+#! naked ones using the $ operator
+
+onready var bad_unique_nodepath_a = $%Unique
+onready var bad_unique_nodepath_b = $Child/%Unique
+onready var bad_unique_nodepath_c = $Child/GrandChild/%Unique
+onready var bad_unique_nodepath_c = $Child/%Unique/ChildOfUnique
+
+onready var node_i = $"%Unique"
+onready var node_ii = get_node("%Unique")
+onready var node_iii = NodePath("%Unique")
+onready var node_j = $'%Unique/Child'
+onready var node_jj = get_node('%Unique/Child')
+onready var node_jjj = NodePath('%Unique/Child')
+onready var node_k = $"%Unique/%UniqueChild"
+onready var node_kk = get_node("%Unique/%UniqueChild")
+onready var node_kkk = NodePath("%Unique/%UniqueChild")
+
+if has_node('%Unique') and get_node('%Child').has_node('%GrandChild'):
+	pass
+
 onready var node_i = $badlyNamedChild
 onready var node_j = $badlyNamedChild/badly_named_grandchild
 
