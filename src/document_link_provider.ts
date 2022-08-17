87a54f963931aacf8220bb8a195bf0b320ff15e5
David Kincaid <daelonsuzuka@gmail.com>
2022-08-16 21:57:11 -0400

    Add document link provider

---
+++ src/document_link_provider.ts
@@ -0,0 +1,57 @@
+import * as vscode from "vscode";
+import { Uri, Position, Range, TextDocument } from "vscode";
+
+export class GDDocumentLinkProvider implements vscode.DocumentLinkProvider {
+	private context: vscode.ExtensionContext;
+
+	constructor(context: vscode.ExtensionContext) {
+		this.context = context;
+
+		const disp = vscode.languages.registerDocumentLinkProvider(
+			["gdscript", "gdresource"],
+			this
+		);
+
+		context.subscriptions.push(disp);
+	}
+
+	async provideDocumentLinks(
+		document: vscode.TextDocument,
+		token: vscode.CancellationToken
+	): Promise<vscode.DocumentLink[]> {
+		let links = [];
+		let lines = document.getText().split("\n");
+		for (let i = 0; i < lines.length; i++) {
+			const match = lines[i].match(/res:\/\/[^"]*/);
+			if (match) {
+				const start = new Position(i, match.index);
+				const end = new Position(i, match.index + match[0].length);
+				const r = new Range(start, end);
+				const uri = await resourcePathToUri(match[0], document);
+				if (uri instanceof Uri) {
+					links.push(new vscode.DocumentLink(r, uri));
+				}
+			}
+		}
+		return links;
+	}
+
+	async resolveDocumentLink(
+		link: any,
+		token: vscode.CancellationToken
+	): Promise<vscode.DocumentLink> {
+		return;
+	}
+}
+
+async function resourcePathToUri(
+	resPath: string,
+	document: vscode.TextDocument
+) {
+	const files = await vscode.workspace.findFiles("**/project.godot");
+	if (!files) {
+		return resPath;
+	}
+	const project_dir = files[0].fsPath.replace("project.godot", "");
+	return Uri.joinPath(Uri.file(project_dir), resPath.substring(6));
+}
