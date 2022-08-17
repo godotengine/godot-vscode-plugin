c660b13921e87ac8b6e0914e143f2ae5c6b8d3c5
David Kincaid <daelonsuzuka@gmail.com>
2022-08-17 11:24:27 -0400

    Added more example code

--- syntaxes/examples/gdscript2.gd
+++ syntaxes/examples/gdscript2.gd
@@ -3,6 +3,15 @@ class_name TestClass2
 
 # ******************************************************************************
 
+func f():
+    await $Button.button_up
+    super()
+    super.some_function()
+    for i in range(1): # `in` is a control keyword
+        print(i in range(1)) # `in` is an operator keyword
+
+# ------------------------------------------------------------------------------
+
 signal changed(new_value)
 var warns_when_changed = "some value":
 	get:
