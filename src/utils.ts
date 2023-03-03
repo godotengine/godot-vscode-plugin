26b55ed8b3427b0e65cf273fa232ef2d183f7a23
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2023-03-02 22:37:01 -0600

    feature: add get_godot_version function

--- src/utils.ts
+++ src/utils.ts
@@ -41,3 +41,21 @@ export async function convert_resource_path_to_uri(resPath: string): Promise<vsc
 	const project_dir = files[0].fsPath.replace("project.godot", "");
 	return vscode.Uri.joinPath(vscode.Uri.file(project_dir), resPath.substring(6));
 }
+export async function get_config_version() {
+	const [configFile] = await vscode.workspace.findFiles("**/project.godot");
+	const configData = fs.readFileSync(configFile.fsPath, "utf-8");
+	const lines = configData.split("\n");
+	const configVersionLine = lines.find((line) =>
+		line.startsWith("config_version=")
+	);
+	const configVersion = configVersionLine.split("=")[1];
+	return parseInt(configVersion);
+}
+export async function get_godot_version() {
+	// Find godot config
+	const configVersion = await get_config_version();
+	if (configVersion >= 5) { // godot 4 uses config version 5
+		return 4;
+	}
+	return 3;
+}
