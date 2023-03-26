58d3f0fa6148b96e9e8202f0f4a7228a29b59e9b
David Kincaid <daelonsuzuka@gmail.com>
2023-03-26 19:21:51 -0400

    Fix debugger version references

--- src/debugger/godot4/debugger_context.ts
+++ src/debugger/godot4/debugger_context.ts
@@ -25,7 +25,7 @@ import { Mediator } from "./mediator";
 export function register_debugger(context: ExtensionContext) {
 	let provider = new GodotConfigurationProvider();
 	context.subscriptions.push(
-		debug.registerDebugConfigurationProvider("godot", provider)
+		debug.registerDebugConfigurationProvider("godot4", provider)
 	);
 
 	let inspector_provider = new InspectorProvider();
@@ -177,7 +177,7 @@ class GodotConfigurationProvider implements DebugConfigurationProvider {
 		if (!config.type && !config.request && !config.name) {
 			const editor = window.activeTextEditor;
 			if (editor && fs.existsSync(`${folder.uri.fsPath}/project.godot`)) {
-				config.type = "godot";
+				config.type = "godot4";
 				config.name = "Debug Godot";
 				config.request = "launch";
 				config.project = "${workspaceFolder}";
