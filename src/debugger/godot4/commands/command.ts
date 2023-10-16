94803b331ce4d116c1b21b996bc293613bccb689
David Kincaid <daelonsuzuka@gmail.com>
2023-10-16 15:41:09 -0400

    Remove unused files

--- src/debugger/godot4/commands/command.ts
+++
@@ -1,5 +0,0 @@
-import { Mediator } from "../mediator";
-
-export abstract class Command {
-	public abstract trigger(parameters: any[]): void;
-}
--- src/debugger/godot4/commands/commands/command_output.ts
+++
@@ -1,9 +0,0 @@
-import { Command } from "../command";
-import { Mediator } from "../../mediator";
-
-export class CommandOutput extends Command {
-	public trigger(parameters: any[]) {
-		let lines: string[] = parameters;
-		Mediator.notify("output", lines);
-	}
-}
