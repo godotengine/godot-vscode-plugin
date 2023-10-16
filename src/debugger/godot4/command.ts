287732b4f9201829d4cc2d665394533ade3d8678
David Kincaid <daelonsuzuka@gmail.com>
2023-10-16 16:59:36 -0400

    Extract command.ts

--- src/debugger/godot4/command.ts
+++
@@ -1,8 +0,0 @@
-
-export class Command {
-	public command: string = "";
-	public param_count: number = -1;
-	public parameters: any[] = [];
-	public complete: boolean = false;
-	public threadId: number = 0;
-}
