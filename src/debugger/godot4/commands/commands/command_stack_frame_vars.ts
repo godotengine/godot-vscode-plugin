97ca2125edb901ea9eb9bb83a7904b56e3bcb84b
David Kincaid <daelonsuzuka@gmail.com>
2023-10-16 15:41:09 -0400

    Remove unused files

--- src/debugger/godot4/commands/commands/command_stack_frame_vars.ts
+++
@@ -1,9 +0,0 @@
-import { Command } from "../command";
-import { Mediator } from "../../mediator";
-
-export class CommandStackFrameVars extends Command {
-	public trigger(parameters: any[]) {
-		let amount = parameters[0];
-		Mediator.notify("stack_frame_vars", [amount]);
-	}
-}
