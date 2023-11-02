8be2700e3f3a946f83985aff9f90d93a3e382050
David Kincaid <daelonsuzuka@gmail.com>
2023-11-02 09:02:14 -0400

    Switch to lezer

--- src/formatter/formatter.ts
+++ src/formatter/formatter.ts
@@ -7,9 +7,42 @@ import {
 } from "vscode";
 import { execSync } from "child_process";
 import { get_configuration } from "../utils";
+import { parser } from "./parser.js"
 import { createLogger } from "../logger";
 
 const log = createLogger("formatter");
+
+
+function format_document(document: TextDocument) {
+	const text = document.getText();
+	const tree = parser.parse(text);
+
+	const cursor = tree.cursor();
+
+	let output = "";
+
+	cursor.iterate((node) => {
+
+		if (node.name === "Script" || node.name === "FunctionDefinition" || node.name === "Body" || node.name === "ParamList" || node.name.includes("Statement") || node.name.includes("Expression")) {
+			return;
+		}
+		log.debug(node.name, text.slice(node.from, node.to));
+
+		// switch (node.name) {
+		// 	case value:
+
+		// 		break;
+
+		// 	default:
+		// 		break;
+		// }
+		output += text.slice(node.from, node.to);
+	});
+
+
+	log.debug("Output:\n", output);
+}
+
 
 export class FormattingProvider implements DocumentFormattingEditProvider {
 	constructor(context: ExtensionContext) {
@@ -31,10 +64,7 @@ export class FormattingProvider implements DocumentFormattingEditProvider {
 				return;
 			}
 			if (formatter === "builtin") {
-				// Not implemented yet
-				// const command = `${formatterPath} ${document.uri.fsPath}`;
-				// const output = execSync(`${command} `);
-				// res([]);
+				format_document(document);
 			}
 			rej();
 		});
