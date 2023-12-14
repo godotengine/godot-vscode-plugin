833cdaa50e1ea5bbde9fe6f8ea1b62e53ddd9ce5
Sandy Gutierrez <sandygk@gmail.com>
2023-12-14 09:48:01 -0500

    Rename at-and-percent to node-paths and use the $ instead of @

--- src/formatter/snapshots/node-paths/in.gd
+++ src/formatter/snapshots/node-paths/in.gd
@@ -3,10 +3,10 @@ extends Node2D
 @onready var sprite: Sprite2D = %Sprite
 @onready var sprites = [ %Sprite1,%Sprite2,%Sprite3  ]
 
-@onready var sprite_name = @'Sprite'
-@onready var sprite_names = [@'Sprite1',    @'Sprite2',   @'Sprite3']
+@onready var sprite_name = $Sprite
+@onready var sprite_names = [$Sprite1,    $Sprite2,   $Sprite3]
 
 func f():
-	print("@'Sprite1'", @'Sprite1')
+	print("$Sprite1", $Sprite1)
 	print("%Sprite1", %Sprite1)
 	var a=val % otherVal
