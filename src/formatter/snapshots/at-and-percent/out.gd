691805451e63bf870a641e6f5997f6c7f789ffdc
David Kincaid <daelonsuzuka@gmail.com>
2023-12-14 03:14:01 -0500

    Fix typo

--- src/formatter/snapshots/at-and-percent/out.gd
+++ src/formatter/snapshots/at-and-percent/out.gd
@@ -7,6 +7,6 @@ extends Node2D
 @onready var sprite_names = [@'Sprite1', @'Sprite2', @'Sprite3']
 
 func f():
-	print("@Sprite1", @'Sprite1')
+	print("@'Sprite1'", @'Sprite1')
 	print("%Sprite1", %Sprite1)
 	var a = val % otherVal
