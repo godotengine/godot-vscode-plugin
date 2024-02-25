383477711ac2e3ce041666c0c41912e719b87472
Daelon Suzuka <daelonsuzuka@gmail.com>
2024-02-25 17:47:32 -0500

    Ignore biome warning

--- src/scene_tools/parser.ts
+++ src/scene_tools/parser.ts
@@ -12,6 +12,7 @@ export class SceneParser {
 
 	constructor() {
 		if (SceneParser.instance) {
+			// biome-ignore lint/correctness/noConstructorReturn: <explanation>
 			return SceneParser.instance;
 		}
 		SceneParser.instance = this;
