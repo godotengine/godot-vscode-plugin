9cfc112a57e7ad9e3dd19a1876139293712d816e
David Kincaid <daelonsuzuka@gmail.com>
2024-11-03 10:37:29 -0500

    Improve scientific notation test

--- src/formatter/snapshots/scientific_notation.gd
+++ src/formatter/snapshots/scientific_notation.gd
@@ -1,9 +1,6 @@
 # --- IN ---
-var  a  =   1e-6
-# --- OUT ---
 var a = 1e-6
-
-# --- IN ---
-var  b  =   58.1e-10
-# --- OUT ---
-var b = 58.1e-10
+var b = 4e-09
+var c = 58.1e-10
+var d = 58.1e+10
+var e = 9.732e-06
