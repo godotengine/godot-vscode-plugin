5afba0b991358b1f9281aee15fd7e75e35d66d43
DaelonSuzuka <daelonsuzuka@gmail.com>
2025-01-03 22:14:40 -0500

    Improve behavior on edge cases

--- src/providers/document_drops.ts
+++ src/providers/document_drops.ts
@@ -41,17 +41,16 @@ export class GDDocumentDropEditProvider implements DocumentDropEditProvider {
 		// "main" script for that scene?
 		// Attempt to calculate a relative path that resolves correctly?
 
-		const path: string = dataTransfer.get("godot/path")?.value;
-		if (path) {
-			const className: string = dataTransfer.get("godot/class")?.value;
-			const line = document.lineAt(position.line);
+		const className: string = dataTransfer.get("godot/class")?.value;
+		if (className) {
+			const path: string = dataTransfer.get("godot/path")?.value;
 			const unique = dataTransfer.get("godot/unique")?.value === "true";
 			const label: string = dataTransfer.get("godot/label")?.value;
+
 			// For the root node, the path is empty and needs to be replaced with the node name
 			const savePath = path || label;
 
 			if (document.languageId === "gdscript") {
-				const snippet = new vscode.SnippetString();
 				let qualifiedPath = `$${savePath}`;
 
 				if (unique) {
@@ -59,6 +58,7 @@ export class GDDocumentDropEditProvider implements DocumentDropEditProvider {
 					qualifiedPath = `%${label}`;
 				}
 
+				const line = document.lineAt(position.line);
 				if (line.text === "") {
 					// We assume that if the user is dropping a node in an empty line, they are at the top of
 					// the script and want to declare an onready variable
