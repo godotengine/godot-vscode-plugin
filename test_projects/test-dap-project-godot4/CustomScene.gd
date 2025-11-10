f78c65ba8fda5c581aadde3acef284696db711e0
MichaelXt <miklmailplus@gmail.com>
2025-11-10 11:27:20 +0700

    CI: Godot version bump to 4.5.1 Test project: update test proj

--- test_projects/test-dap-project-godot4/CustomScene.gd
+++ test_projects/test-dap-project-godot4/CustomScene.gd
@@ -1,9 +1,24 @@
 extends HBoxContainer
 
-class_name CustomSceneClass
+class_name CustomScene
 
-var custom_scene_class_member1 = "CustomSceneClass::member1"
+var custom_scene_class_member1 = "CustomScene::member1"
 
 var __repr__: String:
   get():
-    return "CustomSceneClass %s" % [self.get_instance_id()]
+    return "CustomScene %s" % [self.get_instance_id()]
+
+class CustomSubClass:
+  var __repr__: String:
+    get():
+      # return "CustomSubClass(%s)<%s>" % [some_val, (self.get_instance_id())]
+      return "CustomSubClass(%s)" % [some_val]
+  
+  var some_val: String
+  func _init(some_val: String) -> void:
+    self.some_val = some_val
+
+func _ready() -> void:
+  var arr = [CustomSubClass.new("Hello"), CustomSubClass.new("World")]
+  var dict = {CustomSubClass.new("Hello"): 1, CustomSubClass.new("World"): 2}
+  pass
--- test_projects/test-dap-project-godot4/NodeVars.gd
+++ test_projects/test-dap-project-godot4/NodeVars.gd
@@ -3,9 +3,9 @@ extends Node2D
 @onready var node_1: Node = $node1
 @onready var node_2: Node = $node2
 
-@onready var custom_scene: CustomSceneClass = $CustomScene
-@onready var scenes_array: Array[CustomSceneClass] = [custom_scene, custom_scene]
-@onready var scenes_map: Dictionary[CustomSceneClass, int] = {custom_scene: 3}
+@onready var custom_scene: CustomScene = $CustomScene
+@onready var scenes_array: Array[CustomScene] = [custom_scene, custom_scene]
+@onready var scenes_map: Dictionary[CustomScene, int] = {custom_scene: 3, custom_scene: 5}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
